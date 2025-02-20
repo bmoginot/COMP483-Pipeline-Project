ecca5cdedf2249949855e85e68213151843569b3
Ben Moginot <bmoginot5@gmail.com>
2025-02-20 15:24:50 -0600

    abstracted

--- wrapper.py
+++ wrapper.py
@@ -3,40 +3,51 @@ import glob
 import subprocess
 import re
 
-os.system("rm -r PipelineProject_Ben_Moginot/*")
+    ### EFETCH FOR TARGET ORGANISM SOMEHOW MAYBE ###
 
-os.chdir("PipelineProject_Ben_Moginot")
+def download_cds(acc):
+    os.mkdir("temp") # store ncbi datasets temporarily
+    os.chdir("temp")
+    os.system(f"datasets download genome accession {acc} --include cds") # get all protein sequences for herpesvirus
+    os.system("unzip ncbi_dataset.zip")
+    os.system(f"mv ncbi_dataset/data/{acc}/cds_from_genomic.fna ../herpes.fasta") # get fasta file
+    os.chdir("..")
+    os.system("rm -r temp") # get rid of everything else
+    return
 
-log = open("PipelineProject.log", "w")
+def count_cds():
+    with open("herpes.fasta") as f: # count number of CDS
+        cds = len(re.findall(">", f.read()))
+    return f"The HCMV genome (NC_006273.2) has {str(cds)} CDS"
 
-### EFETCH FOR TARGET ORGANISM SOMEHOW MAYBE ###
+def run_kallisto(ind):
+    os.system(f"kallisto index -i {ind} herpes.fasta") # build kallisto index from protein sequences
+    os.mkdir("results")
+    paths = sorted(glob.glob("../data/fastq_files/*")) # get all fastq files
+    for i in range(0, len(paths), 2): # get each pair of files
+        pair1 = paths[i]
+        pair2 = paths[i+1]
+        print(pair1, pair2)
+        tag = pair1.split("/")[-1].split("_")[0] # get the srr acc for the output dir
+        print(tag)
+        os.mkdir(f"results/{tag}") # make output dir to store results for each pair
+        os.system(f"kallisto quant -i {ind} -o results/{tag} -b 10 -t 2 {pair1} {pair2}") # run kallisto on each pair
+    return
 
-acc = "GCA_000845245.1"
-os.mkdir("temp") # store ncbi datasets temporarily
-os.chdir("temp")
-os.system(f"datasets download genome accession {acc} --include cds") # get all protein sequences for herpesvirus
-os.system("unzip ncbi_dataset.zip")
-os.system(f"mv ncbi_dataset/data/{acc}/cds_from_genomic.fna ../herpes.fasta") # get fasta file
-os.chdir("..")
-os.system("rm -r temp") # get rid of everything else
+def main():
+    os.system("rm -r PipelineProject_Ben_Moginot/*")
+    os.chdir("PipelineProject_Ben_Moginot")
 
-with open("herpes.fasta") as f: # count number of CDS
-    cds = len(re.findall(">", f.read()))
+    log = open("PipelineProject.log", "w")
 
-log.write(f"The HCMV genome (NC_006273.2) has {str(cds)} CDS") # append to log
+    acc = "GCA_000845245.1"
+    download_cds(acc)
+    log.write(count_cds())
 
-index = "herpes.idx"
-os.system(f"kallisto index -i {index} herpes.fasta") # build kallisto index from protein sequences
+    index = "herpes.idx"
+    run_kallisto(index)
 
-os.mkdir("results")
-paths = sorted(glob.glob("../data/fastq_files/*")) # get all fastq files
-for i in range(0, len(paths), 2): # get each pair of files
-    pair1 = paths[i]
-    pair2 = paths[i+1]
-    print(pair1, pair2)
-    tag = pair1.split("/")[-1].split("_")[0] # get the srr acc for the output dir
-    print(tag)
-    os.mkdir(f"results/{tag}") # make output dir to store results for each pair
-    os.system(f"kallisto quant -i {index} -o results/{tag} -b 10 -t 2 {pair1} {pair2}") # run kallisto on each pair
+    log.close()
 
-log.close()
+if __name__ == "__main__":
+    main()
