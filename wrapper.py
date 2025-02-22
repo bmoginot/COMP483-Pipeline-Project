bd8784487b76eaad81d168fba7152aaf387a9fee
Ben Moginot <bmoginot5@gmail.com>
2025-02-22 13:58:40 -0600

    gets sleuth results

--- wrapper.py
+++ wrapper.py
@@ -16,10 +16,12 @@ def download_cds(acc):
     os.system("rm -r temp") # get rid of everything else
     return
 
-def count_cds():
+def count_cds(log):
     with open("herpes.fasta") as f: # count number of CDS
         cds = len(re.findall(">", f.read())) # use regedx matching to find individual cds
-    return f"The HCMV genome (NC_006273.2) has {str(cds)} CDS\n\n"
+    with open(log, "w") as log:
+        log.write("The HCMV genome (NC_006273.2) has {str(cds)} CDS\n\n")
+    return
 
 def run_kallisto(ind):
     os.system(f"kallisto index -i {ind} herpes.fasta") # build kallisto index from protein sequences
@@ -28,50 +30,62 @@ def run_kallisto(ind):
     for i in range(0, len(paths), 2): # get each pair of files
         pair1 = paths[i]
         pair2 = paths[i+1]
-        print(pair1, pair2)
         tag = pair1.split("/")[-1].split("_")[0] # get the srr acc for the output dir
-        print(tag)
         os.mkdir(f"results/{tag}") # make output dir to store results for each pair
         os.system(f"kallisto quant -i {ind} -o results/{tag} -b 10 -t 2 {pair1} {pair2}") # run kallisto on each pair
     return
 
-def tpm_stats(log):
+def write_tpm_stats(log):
     header = ["sample", "condition", "min_tpm", "med_tpm", "mean_tpm", "max_tpm"]
-    log.write("\t".join(header) + "\n")
-
     samples = ["Donor 1", "Donor 1", "Donor 3", "Donor 3"]
     conditions = ["2dpi", "6dpi", "2dpi", "6dpi"]
-
-    results = sorted(glob.glob("results/*"))
-    for i in range(len(results)):
-        r = results[i]
-        tpm = pd.read_csv(f"{r}/abundance.tsv", sep="\t")["tpm"]
-        row = [samples[i], conditions[i], str(tpm.min()), str(tpm.median()), str(tpm.mean()), str(tpm.max())]
-        log.write("\t".join(row) + "\n")
-    log.write("\n")
+    with open(log, "a") as log:
+        log.write("\t".join(header) + "\n")
+        results = sorted(glob.glob("results/*"))
+        for i in range(len(results)):
+            r = results[i]
+            tpm = pd.read_csv(f"{r}/abundance.tsv", sep="\t")["tpm"]
+            row = [samples[i], conditions[i], str(tpm.min()), str(tpm.median()), str(tpm.mean()), str(tpm.max())]
+            log.write("\t".join(row) + "\n")
+        log.write("\n")
     return
 
 def run_sleuth():
-    print("here")
+    results = sorted(glob.glob("results/*"))
+    conditions = ["2dpi", "6dpi", "2dpi", "6dpi"]
+    header = ["sample", "condition", "path"]
+    with open("sleuth_input.txt", "w") as out:
+        out.write("\t".join(header) + "\n")
+        for i in range(len(results)):
+            path = results[i]
+            sample = path.split("/")[-1]
+            out.write("\t".join([sample, conditions[i], path]) + "\n")
+    os.system("Rscript ../run_sleuth.R")
+
+def write_diff_exp(log):
+    df = pd.read_csv("sleuth_results.txt", sep=" ")
+    out = pd.DataFrame(data={"target_id": df["target_id"],
+    "test_stat": df["test_stat"],
+    "pval": df["pval"],
+    "qval": df["qval"]})
+    out.to_csv(log, mode="a", sep="\t", index=False)
 
 def main():
-    # rm -r PipelineProject_Ben_Moginot/*
     os.chdir("PipelineProject_Ben_Moginot")
 
-    log = open("PipelineProject.log", "w")
+    log = "PipelineProject.log"
 
-    """acc = "NC_006273.2"
+    acc = "NC_006273.2"
     download_cds(acc)
-    log.write(count_cds())
+    count_cds(log)
 
     index = "herpes.idx"
     run_kallisto(index)
 
-    tpm_stats(log)"""
+    write_tpm_stats(log)
 
     run_sleuth()
-
-    log.close()
+    write_diff_exp(log)
 
 if __name__ == "__main__":
     main()
