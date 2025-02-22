8504205ff424be19245142de0d0965d62808fa09
Ben Moginot <bmoginot5@gmail.com>
2025-02-22 13:35:31 -0600

    cleaned up code

--- run_sleuth.R
+++ run_sleuth.R
@@ -17,8 +17,4 @@ sleuth_table <- sleuth_results(so, "reduced:full", "lrt", show_all=FALSE) # extr
 
 sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05) |> dplyr::arrange(pval) # filter most significant results, sort by pval
 
-head(sleuth_significant, n=10) # print top 10 transcripts
-
-write.table(sleuth_significant, file="fdr05_results.txt", quote=FALSE, row.names=FALSE) # write most significant transcripts to file
-
-head(dplyr::select(sleuth_significant, target_id, pval, qval), n=10) # just show transcript, pval, and qval
+write.table(sleuth_significant, file="sleuth_results.txt", quote=FALSE, row.names=FALSE) # write most significant transcripts to file
